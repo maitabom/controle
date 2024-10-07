d8ca3f9f3b72444a3b918b01fc3c2c7dcb179d5a
Fábio Valentim <maitabom@gmail.com>
2024-10-06 21:00:09 -0300

    Criação de página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 
+import heroImg from "@/assets/hero.svg";
+
 export default function Home() {
   return (
-    <main>
-      
+    <main className="flex items-center flex-col justify-center min-h-heightscreen">
+      <h2 className="font-medium text-2xl mb-2">Gerencie sua Empresa</h2>
+      <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">Atendimentos em Clientes</h1>
+      <Image
+        src={heroImg}
+        alt="Imagem de desenvolvimento do controle"
+        width={600}
+        className="max-w-sm md:max-w-xl"
+      />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      spacing: {
+        "heightscreen": "calc(100vh - 80px)"
+      }
     },
   },
   plugins: [],
