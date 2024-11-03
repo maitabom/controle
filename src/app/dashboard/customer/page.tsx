22c74aa8bedca46f6f852a4f702043fae98dd8d9
Fábio Valentim <maitabom@gmail.com>
2024-11-02 23:43:07 -0300

    Criação de página de listagem de clientes

--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -1,13 +1,23 @@
 import Container from "@/components/container";
+import Link from "next/link";
+import CardCustomer from "./components/card";
 
 export default function DashboardCustomer() {
   return (
     <Container>
-      <main>
-        <div>
-          <h1>Meus clientes</h1>
+      <div className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Meus clientes</h1>
+          <Link href="/dashboard/customer/new" className="bg-blue-500 text-white px-4 py-1 rounded">
+            Novo Cliente
+          </Link>
         </div>
-      </main>
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-2">
+          <CardCustomer />
+          <CardCustomer />
+          <CardCustomer />
+        </section>
+      </div>
     </Container>
   )
 }
