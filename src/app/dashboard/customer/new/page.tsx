9c5f376a0df55833cde49396be717f953e0e6087
Fábio Valentim <maitabom@gmail.com>
2024-11-03 16:42:23 -0300

    Finalização de tela de cadastro de cliente

--- src/app/dashboard/customer/new/page.tsx
+++ src/app/dashboard/customer/new/page.tsx
@@ -1,8 +1,12 @@
 import Container from "@/components/container";
 import Link from "next/link";
 import NewCustomerForm from "../components/form";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export default function NewCustomer() {
+export default async function NewCustomer() {
+  const session = await getServerSession(authOptions);
+
   return (
     <Container>
       <div className="flex flex-col mt-9 mb-2">
@@ -10,7 +14,7 @@ export default function NewCustomer() {
           <Link href="/dashboard/customer" className="bg-gray-900 px-4 py-1 rounded text-white">Voltar</Link>
           <h1 className="text-3xl font-bold">Novo Cliente</h1>
         </div>
-        <NewCustomerForm />
+        <NewCustomerForm userId={session?.user.id} />
       </div>
     </Container>
   )
